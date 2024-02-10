684d94f6edaf06edb183e8273b1a73bf92e166e6
giri <girikrishna.p6@gmail.com>
2024-02-10 09:57:21 +0530

    hello

--- Back-end/util/multerConfig.js
+++ Back-end/util/multerConfig.js
@@ -5,18 +5,18 @@ const path = require('path');
 // Set up multer for handling file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        console.log('multer cover - ', req.body.cover);
-       
+        console.log('multer cover - ', req?.body?.cover);
+        if (!req.body.cover) {
             console.log("hello -------------------")
             cb(null, 'public/NovelCovers/');
-      
+        }
     },
     filename: function (req, file, cb) {
- 
+        if (!req.body.cover) {
             console.log("hello2 ------------------")
             const NovelFileName = `${req.body.title}.jpeg`;
             cb(null, NovelFileName);
-    
+        }
     },
 })
 
