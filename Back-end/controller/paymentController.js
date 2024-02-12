6e9816ffb46992865934ce3832a709afc18c2717
giri <girikrishna.p6@gmail.com>
2024-02-12 14:50:44 +0530

    fix amountAdd History

--- Back-end/controller/paymentController.js
+++ Back-end/controller/paymentController.js
@@ -67,7 +67,7 @@ module.exports = {
                 console.log('the payment is successful ⭐⭐⭐⭐');
 
                 let AddHistory = {
-                    amountAdd: session.amount_total / 100,
+                    amount: session.amount_total / 100,
                     date: new Date()
                 };
 
