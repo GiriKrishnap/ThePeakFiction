ab66a3ce6921586dfe04490bea42f3918496b870
giri <girikrishna.p6@gmail.com>
2024-02-12 10:20:44 +0530

    multer

--- Back-end/util/multerConfig.js
+++ Back-end/util/multerConfig.js
@@ -1,3 +1,41 @@
+// // multerConfig.js
+// const multer = require('multer');
+// const path = require('path');
+
+// // Set up multer for handling file uploads
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         // Handle potential errors
+//         if (!file) {
+//             console.log('No file received');
+//             return cb(new Error('No file received'));
+//         }
+//         cb(null, path.join(__dirname, '../public/NovelCovers'));
+//     },
+
+//     filename: function (req, file, cb) {
+//         // Handle potential errors
+//         if (!file || !req.body.title) {
+//             console.log('Invalid file or missing title')
+//             return cb(new Error('Invalid file or missing title'));
+//         }
+//         // Dynamically determine file extension based on MIME type
+//         const fileExtension = file.mimetype.split('/')[1];
+//         console.log(fileExtension);
+//         const novelFileName = `${req.body.title}.${fileExtension}`;
+//         console.log('file name is here - ', novelFileName);
+
+//         cb(null, novelFileName);
+//     }
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
+
+
+
 // multerConfig.js
 const multer = require('multer');
 const path = require('path');
@@ -5,29 +43,18 @@ const path = require('path');
 // Set up multer for handling file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // Handle potential errors
-        if (!file) {
-            console.log('No file received');
-            return cb(new Error('No file received'));
+        console.log('multer cover - ', req.body.cover);
+        if (!req.body.cover) {
+            cb(null, 'public/novelCovers/');
         }
-        cb(null, path.join(__dirname, '../public/NovelCovers'));
     },
-
     filename: function (req, file, cb) {
-        // Handle potential errors
-        if (!file || !req.body.title) {
-            console.log('Invalid file or missing title')
-            return cb(new Error('Invalid file or missing title'));
+        if (!req.body.cover) {
+            const NovelFileName = `${req.body.title}.jpeg`;
+            cb(null, NovelFileName);
         }
-        // Dynamically determine file extension based on MIME type
-        const fileExtension = file.mimetype.split('/')[1];
-        console.log(fileExtension);
-        const novelFileName = `${req.body.title}.${fileExtension}`;
-        console.log('file name is here - ', novelFileName);
-
-        cb(null, novelFileName);
-    }
-});
+    },
+})
 
 const upload = multer({ storage: storage });
 
