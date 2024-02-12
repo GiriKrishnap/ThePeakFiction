05e658e404367557477af3e1a2fb2ba6759b46d7
giri <girikrishna.p6@gmail.com>
2024-02-12 11:13:30 +0530

    adminNovelCOver

--- Back-end/controller/adminController.js
+++ Back-end/controller/adminController.js
@@ -146,7 +146,7 @@ module.exports = {
 
                 if (novel) {
 
-                    const imagePath = path.join(__dirname, '..', novel.cover);
+                    const imagePath = path.join(__dirname, novel.cover);
                     res.contentType('image/jpeg');
                     res.sendFile(imagePath);
 
