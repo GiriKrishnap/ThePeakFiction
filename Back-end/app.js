0e65747a86a0dccc92d4dc7083ebd35b87981b8c
giri <girikrishna.p6@gmail.com>
2024-02-12 10:43:22 +0530

    sd

--- Back-end/app.js
+++ Back-end/app.js
@@ -23,6 +23,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'NovelCovers')));
 
