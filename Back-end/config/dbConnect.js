d0248bd8630eb13ed63b2658b54094179ee834b0
Giri Krishna <127300656+GiriKrishnap@users.noreply.github.com>
2024-05-09 14:25:27 +0530

    Update dbConnect.js

--- Back-end/config/dbConnect.js
+++ Back-end/config/dbConnect.js
@@ -1,15 +1,18 @@
 const { default: mongoose } = require("mongoose");
 
 const dbConnect = async () => {
-    try {
-        await mongoose.connect(process.env.CONNECTION_STRING, {
-  socketTimeoutMS: 30000, // Increase the timeout value to 30 seconds
-  // Other options...
-})
-        console.log("mongodb connected successfully");
-    } catch (error) {
-        console.log('error on mongoDb connection', error)
-    }
-}
+  try {
+    const connectionString = process.env.CONNECTION_STRING
+
+    await mongoose.connect(connectionString, {
+      socketTimeoutMS: 30000, // Increase the timeout value to 30 seconds
+      // Other options...
+    });
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log("Error on MongoDB connection", error);
+  }
+};
 
 module.exports = dbConnect;
